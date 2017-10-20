691556bf4bf9a884b9802bb37825a61d3f1c0c93
olgavrou <olgavrou@gmail.com>
2017-10-20 14:25:24 +0100

    Made config location in run.py customizable

--- sumstats/run.py
+++ sumstats/run.py
@@ -5,11 +5,17 @@ import glob
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-config', help='config file', required=True)
+parser.add_argument('-config', help='config file location', required=False)
+args = parser.parse_args()
+if args.config is None:
+    print("Setting default location for configuration file")
+    config_location = "/application/config"
+else:
+    config_location = args.config
 
-config_files = glob.glob("/application/config/config*.yml")
+config_files = glob.glob(config_location + "/config*.yml")
 print (config_files)
-assert len(config_files) == 1, "We should only have one config file in the config directory!"
+assert len(config_files) == 1, "We should have exactly one config file in the config directory!"
 
 config_file = config_files[0]
 with open(config_file, 'r') as ymlfile:
