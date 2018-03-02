c598d1ecbec42c23cef8ac2859f7973e0261669b
olgavrou <olgavrou@gmail.com>
2018-03-02 15:10:41 +0000

    append header doesn't rely on template header anymore

--- bin/utils/append_header.sh
+++ bin/utils/append_header.sh
@@ -3,8 +3,8 @@
 base=$(cd ${0%/*}/../..; pwd)
 
 file=$1
-header=$2
 filename=$(basename $file)
+header=$(head -n 1 $base/$filename)
 
 cd $base/files/toload
 
@@ -13,11 +13,11 @@ do
     chromosome_file=chr"$chr"_"$filename"
     if [ -s $chromosome_file ];
     then
-        echo "$2" | cat - $chromosome_file > temp
+        echo "$header" | cat - $chromosome_file > temp
         mv temp $chromosome_file
     fi
 done
 
-echo "$2" | cat - $filename > temp
+echo "$header" | cat - $filename > temp
 mv temp $filename
 cd $base
