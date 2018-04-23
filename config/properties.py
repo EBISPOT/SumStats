03a368d72fd1914b79c1eaabdda3cc8d029d0737
olgavrou <olgavrou@gmail.com>
2018-04-23 14:21:37 +0100

    GOCI-2184: Making logging more configurable and giving a prefix to the api path

--- config/properties.py
+++ config/properties.py
@@ -9,7 +9,11 @@ ols_terms_location= "https://www.ebi.ac.uk/ols/api/terms?id="
 gwas_study_location="http://wwwdev.ebi.ac.uk/gwas/beta/rest/api/studies/"
 logging_path="./logs"
 port=8080
+APPLICATION_ROOT="/gwas/summary-statistics/api"
 LOG_LEVEL="INFO"
+ACCESS_LOG="access.log"
+ERROR_LOG="error.log"
+LOGGER_LOG="logger.log"
 LOG_CONF = {
     'version': 1,
     'formatters': {
@@ -37,7 +41,6 @@ LOG_CONF = {
             'level':'INFO',
             'class': 'logging.handlers.RotatingFileHandler',
             'formatter': 'standard',
-            'filename': 'access.log',
             'maxBytes': 10485760,
             'backupCount': 20,
             'encoding': 'utf8'
@@ -46,7 +49,6 @@ LOG_CONF = {
             'level':'INFO',
             'class': 'logging.handlers.RotatingFileHandler',
             'formatter': 'standard',
-            'filename': 'errors.log',
             'maxBytes': 10485760,
             'backupCount': 20,
             'encoding': 'utf8'
@@ -55,7 +57,6 @@ LOG_CONF = {
             'level':'INFO',
             'class': 'logging.handlers.RotatingFileHandler',
             'formatter': 'standard',
-            'filename': 'logger.log',
             'maxBytes': 10485760,
             'backupCount': 20,
             'encoding': 'utf8'
--- sumstats/utils/register_logger.py
+++ sumstats/utils/register_logger.py
@@ -1,12 +1,18 @@
 from config import properties
 import logging
 from logging import config
+from sumstats.utils import properties_handler as set_p
 
 
 def register(logger_name):
+    set_p.set_properties()
     LOG_CONF = properties.LOG_CONF
     LOG_CONF['loggers'][logger_name] = {
         'handlers': ['logger'],
         'propagate': False
     }
+    properties.LOG_CONF['handlers']['cherrypy_access'][
+        'filename'] = properties.logging_path + "/" + properties.ACCESS_LOG
+    properties.LOG_CONF['handlers']['cherrypy_error']['filename'] = properties.logging_path + "/" + properties.ERROR_LOG
+    properties.LOG_CONF['handlers']['logger']['filename'] = properties.logging_path + "/" + properties.LOGGER_LOG
     logging.config.dictConfig(LOG_CONF)
