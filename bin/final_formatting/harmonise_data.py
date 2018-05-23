70df4faa1541a30c5ed5ae7932585f689d16560a
olgavrou <olgavrou@gmail.com>
2018-05-23 14:35:59 +0100

    GOCI-2066: refactoring with jdhayhurst

--- bin/final_formatting/harmonise_data.py
+++ bin/final_formatting/harmonise_data.py
@@ -42,29 +42,14 @@ class EnsemblRestClient(object):
         self.last_req = 0
 
     def perform_rest_action(self, endpoint, hdrs=None):
-        if hdrs is None:
-            hdrs = {}
-
-        if 'Content-Type' not in hdrs:
-            hdrs['Content-Type'] = 'application/json'
-
+        hdrs = self.set_json_format(hdrs)
+        self.rate_check()
         data = None
-
-        # check if we need to rate limit ourselves
-        if self.req_count >= self.reqs_per_sec:
-            time.sleep(1)
-            delta = time.time() - self.last_req
-            if delta < 1:
-                time.sleep(1 - delta)
-            self.last_req = time.time()
-            self.req_count = 0
 
         try:
             content = requests.get(self.server + endpoint, headers=hdrs)
-            if 'Retry-After' in content.headers: 
-                retry = content.headers['Retry-After']
-                time.sleep(float(retry))
-                content = requests.get(self.server + endpoint, headers=hdrs) 
+            self.slow_down_if_needed(content.headers)
+            content = requests.get(self.server + endpoint, headers=hdrs)
             if content:
                 data = content.json()
             else:
@@ -74,51 +59,87 @@ class EnsemblRestClient(object):
         except requests.exceptions.RequestException as e:
             # check if we are being rate limited by the server
             if e.code == 429:
-                if 'Retry-After' in e.headers:
-                    retry = e.headers['Retry-After']
-                    time.sleep(float(retry))
-                    self.perform_rest_action(endpoint, hdrs)
+                self.slow_down_if_needed(e.headers)
+                self.perform_rest_action(endpoint, hdrs)
             else:
                 print('Request failed for {0}: Status code: {1.code} Reason: {1.reason}\n'.format(
                 endpoint, e))
 
         return data
 
+    def slow_down_if_needed(self, headers):
+        if 'Retry-After' in headers:
+            retry = headers['Retry-After']
+            time.sleep(float(retry))
+
+    def set_json_format(self, headers):
+        if headers is None:
+            headers = {}
+        if 'Content-Type' not in headers:
+            headers['Content-Type'] = 'application/json'
+        return headers
+
+    def rate_check(self):
+        # check if we need to rate limit ourselves
+        if self.req_count >= self.reqs_per_sec:
+            time.sleep(1)
+            delta = time.time() - self.last_req
+            if delta < 1:
+                time.sleep(1 - delta)
+            self.last_req = time.time()
+            self.req_count = 0
+
     def map_bp_to_build_with_rest(self, chromosome, bp, build_orig, build_mapped):
         map_build = self.perform_rest_action(
             '/map/human/{orig}/{chromosome}:{bp}:{bp}/{mapped}?'.format(
             chromosome=chromosome, bp=bp, orig=build_orig, mapped=build_mapped)
             )
-        try:
-            bp_mapped = map_build["mappings"][0]["mapped"]["start"]
-        except (IndexError, TypeError):
-            bp_mapped = 'NA'
-        return bp_mapped
+        return self.retrieve_mapped_item(map_build)
 
-    def get_rsid(self, chromosome, bp):
+    def retrieve_mapped_item(self, map_build):
+        if "mappings" in map_build:
+            mappings = map_build["mappings"]
+            if len(mappings) > 0:
+                if "mapped" in mappings and "start" in mappings["mapped"]:
+                    return map_build["mappings"][0]["mapped"]["start"]
+        return 'NA'
+
+    def resolve_rsid(self, chromosome, bp):
         rsid_request = self.perform_rest_action(
             '/overlap/region/human/{chromosome}:{bp}:{bp}?feature=variation'.format(
             chromosome=chromosome, bp=bp)
             )
-        try:
+
+        return self.retrieve_rsid(rsid_request)
+
+    def retrieve_rsid(self, rsid_request):
+        if len(rsid_request) > 0 and "id" in rsid_request[0]:
             return rsid_request[0]["id"]
-        except (IndexError, TypeError): 
-            return 'id:NA'
+        return 'id:NA'
 
 
-def get_index(header, col):
-    return header.index(col)
+def isNumber(value):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
 
 
 # Map using pyLiftover and if that fails map using Ensembl REST API
-def map_bp_to_build(chromosome, bp, build_map, build_orig, build_mapped):
-    try:
-        data = build_map.convert_coordinate('chr' + str(chromosome), bp)[0][1]
-    except (IndexError, TypeError):
-        client = EnsemblRestClient()
-        data = client.map_bp_to_build_with_rest(
-            chromosome, bp, suffixed_release.get(build_orig), suffixed_release.get(build_mapped)
-            )
+def map_bp_to_build_via_liftover(chromosome, bp, build_map):
+    if isNumber(bp):
+        data = build_map.convert_coordinate('chr' + str(chromosome), bp)
+        if len(data) > 0:
+            return data[0][1]
+    return None
+
+
+def map_bp_to_build_via_ensembl(chromosome, bp, from_build, to_build):
+    client = EnsemblRestClient()
+    data = client.map_bp_to_build_with_rest(
+        chromosome, bp, suffixed_release.get(from_build), suffixed_release.get(to_build)
+    )
     return data
 
 
@@ -129,14 +150,14 @@ def main():
     argparser.add_argument('-mapped', help='The build to map to', required=True)
     args = argparser.parse_args()
     file = args.f
-    build_orig = args.original
-    build_mapped = args.mapped
+    from_build = args.original
+    to_build = args.mapped
 
     filename = get_filename(file)
     header = None
     is_header = True
     lines = []
-    build_map = LiftOver(ucsc_release.get(build_orig), ucsc_release.get(build_mapped))
+    build_map = LiftOver(ucsc_release.get(from_build), ucsc_release.get(to_build))
     client = EnsemblRestClient()
 
     with open(file) as csv_file:
@@ -146,20 +167,23 @@ def main():
                 header = row
                 is_header = False
             else:
-                chr_index = get_index(header, CHR_DSET)
-                snp_index = get_index(header, SNP_DSET)
-                bp_index = get_index(header, BP_DSET)
+                chr_index = header.index(CHR_DSET)
+                snp_index = header.index(SNP_DSET)
+                bp_index = header.index(BP_DSET)
 
                 chromosome = row[chr_index].replace('23', 'X').replace('24', 'Y')
                 bp = int(row[bp_index])
 
                 # do the bp location mapping
-                row[bp_index] = map_bp_to_build(chromosome, bp, build_map, build_orig, build_mapped)
+                mapped_bp = map_bp_to_build_via_liftover(chromosome=chromosome, bp=bp, build_map=build_map)
+                if mapped_bp is None:
+                    mapped_bp = map_bp_to_build_via_ensembl(chromosome=chromosome, bp=bp, from_build=from_build, to_build=to_build)
+                row[bp_index] = mapped_bp
 
                 # lookup missing rsids
                 bp = row[bp_index]
                 if 'rs' not in row[snp_index]:
-                    row[snp_index] = client.get_rsid(chromosome, bp)
+                    row[snp_index] = client.resolve_rsid(chromosome, bp)
 
                 lines.append(row)
 
