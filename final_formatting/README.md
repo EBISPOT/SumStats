d7d52907ccadfe88ada9eb0fab3ddaa75e2d08f2
James Hayhurst <hayhurst.jd@gmail.com>
2018-05-25 08:29:33 +0100

    refactored harmonise_data.py and README.md

---
+++ final_formatting/README.md
@@ -0,0 +1,31 @@
+# Final formatting tools
+
+Formatters for bringing files from a 'common' to a loadable format. 
+
+The files must first be formatted to the 'common' format using the [sum-stats-formatter](../sum-stats-formatter).
+
+
+# List of tools:
+
+1) `sumstats_formatting.py` brings pre-formatted summary statistics files to a database-loadable format. It replaces headers with database-loadable headers and adds any required headers that are missing. Where a header has been added, the records are populated with 'NA' values.
+
+- `$ python sumstats_formatting.py -f <filename>`
+
+
+2) `harmonise_data.py` performs the following:
+- maps the base-pair location from the originally specified genome to a desired genome build using [PyLiftover](https://pypi.org/project/pyliftover/).
+- resolves any missing RSID's by querying [Ensembl's REST API](https://rest.ensembl.org/).
+- unresolvable base-pair locations and RSID's are assigned 'NA' and 'id:NA' respectively.
+
+- `$ python harmonise_data.py -f <filename> -original <from genome build> -mapped <to genome build>`
+
+
+3) `basic_qc.py` performs the following:
+- checks for 'variant_id', 'p_value', 'chromosome' and 'base_pair_location' columns.
+- removes rows where any of the above are blank
+- check data types
+
+- `$ python basic_qc.py -f <filename>`
+
+
+4) [custom_formatters/](custom_formatters/) contains custom built formatters for specific file formats.
---
+++ final_formatting/harmonise_data.py
@@ -0,0 +1,188 @@
+import requests
+import sys
+import csv
+import argparse
+import time
+from pyliftover import LiftOver
+
+sys_paths = ['sumstats/', '../sumstats/', '../../sumstats/']
+sys.path.extend(sys_paths)
+from sumstats_formatting import *
+
+
+ucsc_release = {'28': 'hg10',
+                '29': 'hg11',
+                '30': 'hg13',
+                '31': 'hg14',
+                '33': 'hg15',
+                '34': 'hg16',
+                '35': 'hg17',
+                '36': 'hg18',
+                '37': 'hg19',
+                '38': 'hg38'}
+
+
+suffixed_release = {'28': 'NCBI28',
+                    '29': 'NCBI29',
+                    '30': 'NCBI30',
+                    '31': 'NCBI31',
+                    '33': 'NCBI33',
+                    '34': 'NCBI34',
+                    '35': 'NCBI35',
+                    '36': 'NCBI36',
+                    '37': 'GRCh37',
+                    '38': 'GRCh38'}
+
+
+class EnsemblRestClient(object):
+    def __init__(self, server='http://rest.ensembl.org', reqs_per_sec=15):
+        self.server = server
+        self.reqs_per_sec = reqs_per_sec
+        self.req_count = 0
+        self.last_req = 0
+
+    def perform_rest_action(self, endpoint, hdrs=None):
+        hdrs = self.set_json_format(hdrs)
+        self.rate_check()
+        data = None
+
+        try:
+            content = requests.get(self.server + endpoint, headers=hdrs)
+            if self.slow_down_if_needed(content.headers):
+                content = requests.get(self.server + endpoint, headers=hdrs)
+            if content:
+                data = content.json()
+            else:
+                data = 'NA'
+            self.req_count += 1
+
+        except requests.exceptions.HTTPError as ehe:
+            # check if we are being rate limited by the server
+            if self.slow_down_if_needed(ehe.headers):
+                self.perform_rest_action(endpoint, hdrs)
+        except requests.exceptions.RequestException as e:
+                print('Request failed for {0}: Status: {1}\n'.format(
+                endpoint, e))
+
+        return data
+
+    def slow_down_if_needed(self, headers):
+        if 'Retry-After' in headers:
+            retry = headers['Retry-After']
+            time.sleep(float(retry))
+            return True
+
+    def set_json_format(self, headers):
+        if headers is None:
+            headers = {}
+        if 'Content-Type' not in headers:
+            headers['Content-Type'] = 'application/json'
+        return headers
+
+    def rate_check(self):
+        # check if we need to rate limit ourselves
+        if self.req_count >= self.reqs_per_sec:
+            time.sleep(1)
+            delta = time.time() - self.last_req
+            if delta < 1:
+                time.sleep(1 - delta)
+            self.last_req = time.time()
+            self.req_count = 0
+
+    def map_bp_to_build_with_rest(self, chromosome, bp, build_orig, build_mapped):
+        map_build = self.perform_rest_action(
+            '/map/human/{orig}/{chromosome}:{bp}:{bp}/{mapped}?'.format(
+            chromosome=chromosome, bp=bp, orig=build_orig, mapped=build_mapped)
+            )
+        return self.retrieve_mapped_item(map_build)
+
+    def retrieve_mapped_item(self, map_build):
+        if "mappings" in map_build:
+            mappings = map_build["mappings"]
+            if len(mappings) > 0:
+                if "mapped" in mappings and "start" in mappings["mapped"]:
+                    return map_build["mappings"][0]["mapped"]["start"]
+        return 'NA'
+
+    def resolve_rsid(self, chromosome, bp):
+        rsid_request = self.perform_rest_action(
+            '/overlap/region/human/{chromosome}:{bp}:{bp}?feature=variation'.format(
+            chromosome=chromosome, bp=bp)
+            )
+        return self.retrieve_rsid(rsid_request)
+
+    def retrieve_rsid(self, rsid_request):
+        if len(rsid_request) > 0 and "id" in rsid_request[0]:
+            return rsid_request[0]["id"]
+        return 'id:NA'
+
+
+def isNumber(value):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
+
+# Map using pyLiftover and if that fails map using Ensembl REST API
+def map_bp_to_build_via_liftover(chromosome, bp, build_map):
+    if isNumber(bp):
+        data = build_map.convert_coordinate('chr' + str(chromosome), int(bp))
+        if len(data) > 0:
+            return data[0][1]
+    return None
+
+
+def map_bp_to_build_via_ensembl(chromosome, bp, from_build, to_build):
+    client = EnsemblRestClient()
+    data = client.map_bp_to_build_with_rest(
+        chromosome, bp, suffixed_release.get(from_build), suffixed_release.get(to_build)
+    )
+    return data
+
+
+def open_file_and_process(file, from_build, to_build):
+    filename = get_filename(file)
+    new_filename = 'harmonised_' + filename + '.tsv'
+    build_map = LiftOver(ucsc_release.get(from_build), ucsc_release.get(to_build))
+
+    with open(file) as csv_file:
+        result_file = open(new_filename, "w")
+        csv_reader = csv.DictReader(csv_file, delimiter='\t')
+        fieldnames = csv_reader.fieldnames
+        writer = csv.DictWriter(result_file, fieldnames=fieldnames, delimiter='\t')
+
+        writer.writeheader()
+
+        for row in csv_reader:
+            chromosome = row[CHR_DSET].replace('23', 'X').replace('24', 'Y')
+            bp = row[BP_DSET]
+            # do the bp location mapping
+            mapped_bp = map_bp_to_build_via_liftover(chromosome=chromosome, bp=bp, build_map=build_map)
+            if mapped_bp is None:
+                mapped_bp = map_bp_to_build_via_ensembl(chromosome=chromosome, bp=bp, from_build=from_build, to_build=to_build)
+            row[BP_DSET] = mapped_bp
+            # lookup missing rsids
+            bp = row[BP_DSET]
+            if 'rs' not in row[SNP_DSET]:
+                client = EnsemblRestClient()
+                row[SNP_DSET] = client.resolve_rsid(chromosome, bp)
+            writer.writerow(row)
+
+
+def main():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('-f', help='The name of the file to be processed', required=True)
+    argparser.add_argument('-original', help='The build of the file', required=True)
+    argparser.add_argument('-mapped', help='The build to map to', required=True)
+    args = argparser.parse_args()
+    file = args.f
+    from_build = args.original
+    to_build = args.mapped
+
+    open_file_and_process(file, from_build, to_build)
+
+
+if __name__ == "__main__":
+    main()
