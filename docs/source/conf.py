0c32623d2a2be584cea00385d516057d4820a027
jdhayhurst <38317975+jdhayhurst@users.noreply.github.com>
2018-10-03 17:10:47 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('../.env/src/sphinx'))
 
 project = 'sumstats'
 copyright = '2018, EMBL-EBI'
-author = 'EMBL-EBI'
+author = 'olgavrou'
 
 # The short X.Y version
 version = ''
