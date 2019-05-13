728b40618006631da31bdb0fa42f0efc35de302f
jdhayhurst <hayhurst.jd@gmail.com>
2019-05-13 16:34:40 +0100

    trait loader and dropping strings over max limit

--- sumstats/load.py
+++ sumstats/load.py
@@ -13,7 +13,9 @@ import sumstats.utils.sqlite_client as sq
 
 
 class Loader():
-    def __init__(self, tsv=None, csv_out=None, var_file=None, qtl_group=None, study_dir=None, study=None, trait=None, hdf_path=None, chromosome=None, expr_file=None, sqldb=None, loader=None, trait_dir=None):
+    def __init__(self, tsv=None, csv_out=None, var_file=None, qtl_group=None, 
+                 study_dir=None, study=None, trait=None, hdf_path=None,
+                 chromosome=None, expr_file=None, sqldb=None, loader=None, trait_dir=None):
         self.tsv = tsv
         self.csv_out = csv_out
         self.var_file = var_file
@@ -26,10 +28,10 @@ class Loader():
         self.trait_dir = trait_dir
         self.expr_file = expr_file
         self.max_string = 255
-
-        self.filename = os.path.splitext(os.path.basename(self.tsv))[0]
+ 
+        if self.tsv:
+            self.filename = os.path.splitext(os.path.basename(self.tsv))[0]
         self.traits = trait
-
 
         self.sqldb = sqldb
 
@@ -44,6 +46,29 @@ class Loader():
     def load_traits(self):
         hdf_store = fsutils.create_h5file_path(path=self.hdf_path, file_name="file_phen_meta", dir_name=self.trait_dir)
         dftrait = pd.read_csv(self.trait_file, sep="\t")
+        self.write_traitfile_to_hdf(hdf_store, os.path.basename(self.trait_file))
+
+
+    def write_traitfile_to_hdf(self, hdf, group):
+        """Read in the trait file"""
+        dftrait = pd.read_csv(self.trait_file, sep="\t")
+        #mapping = {'X': 23, 'x': 23, 'Y': 24, 'y': 24}
+        dftrait[CHR_DSET].replace({'X': 23, 'x': 23, 'Y': 24, 'y': 24}, inplace=True)
+        dftrait[CHR_DSET] = dftrait[CHR_DSET].astype(int)
+
+        #dftrait = pd.to_numeric(dftrait[CHR_DSET], errors='coerce')
+        #['X|x', 'Y|y', 'MT|mt|Mt'], ['23', '24', '25'])
+
+        with pd.HDFStore(hdf) as store:
+            """store in hdf5 as below"""
+            dftrait.to_hdf(store, group,
+                        complib='blosc',
+                        complevel=9,
+                        format='table',
+                        mode='a',
+                        data_columns=['phenotype_id', 'gene_id']
+                        )
+
 
     def write_csv_to_hdf(self, hdf, group):
         """Read in the sumstats files in chunks"""
@@ -88,7 +113,8 @@ class Loader():
                 merged3 = pd.merge(merged2, dfexpr, how='left', left_on=['molecular_trait_id'], right_on=['phenotype_id'])
                 print("merged three")
                 merged3 = merged3[list(TO_LOAD_DSET_HEADERS_DEFAULT)]
-
+                for field in [SNP_DSET, EFFECT_DSET, OTHER_DSET, PHEN_DSET, GENE_DSET, MTO_DSET]:
+                    self.nullify_if_string_too_long(df=merged3, field=field) 
                 merged3.to_hdf(store, group,
                             complib='blosc',
                             complevel=9,
@@ -102,6 +128,7 @@ class Loader():
                                           PHEN_DSET: self.max_string,
                                           GENE_DSET: self.max_string,
                                           MTO_DSET: self.max_string,
+                                          CHR_DSET: 2,
                                           SNP_DSET: self.max_string},
                             index = False
                             )
@@ -119,17 +146,21 @@ class Loader():
                 count += 1
 
 
+    def nullify_if_string_too_long(self, df, field):
+        mask = df[field].str.len() <= self.max_string
+        df[field].where(mask, 'NA', inplace=True)
+
 
 def main():
     argparser = argparse.ArgumentParser()
-    argparser.add_argument('-f', help='The path to the summary statistics file to be processed', required=True)
-    argparser.add_argument('-csv', help='The path to the csv OUT file', required=True)
-    argparser.add_argument('-var', help='The path to the variant/genotype metadata file', required=True)
-    argparser.add_argument('-phen', help='The path to the trait/phenotype metadata file', required=True)
-    argparser.add_argument('-expr', help='The path to the gene expression file', required=True)
-    argparser.add_argument('-study', help='The study identifier', required=True)
-    argparser.add_argument('-qtl_group', help='The qtl group e.g. "LCL"', required=True)
-    argparser.add_argument('-chr', help='The chromosome the data belongs to', required=True)
+    argparser.add_argument('-f', help='The path to the summary statistics file to be processed', required=False)
+    argparser.add_argument('-csv', help='The path to the csv OUT file', required=False)
+    argparser.add_argument('-var', help='The path to the variant/genotype metadata file', required=False)
+    argparser.add_argument('-phen', help='The path to the trait/phenotype metadata file', required=False)
+    argparser.add_argument('-expr', help='The path to the gene expression file', required=False)
+    argparser.add_argument('-study', help='The study identifier', required=False)
+    argparser.add_argument('-qtl_group', help='The qtl group e.g. "LCL"', required=False)
+    argparser.add_argument('-chr', help='The chromosome the data belongs to', required=False)
     argparser.add_argument('-loader', help='The loader', choices=['study', 'trait', 'study_info'], default=None, required=True)
 
     args = argparser.parse_args()
@@ -160,7 +191,7 @@ def main():
             #loader = Loader(filename, tsvfiles_path, chr_dir, study_dir, study, traits, h5files_path, chromosome, database, loader_type)
             loader.load_bystudy()
     elif loader_type == 'trait':
-            loader = Loader(tsv=ss_file, expr_file=expr_file, csv_out=csv_file, var_file=var_file, qtl_group=qtl_group, study_dir=study_dir, study=study, trait=phen_file, hdf_path=h5files_path, chromosome=chromosome, sqldb=database, loader=loader_type, trait_dir=trait_dir)
+            loader = Loader(trait=phen_file, hdf_path=h5files_path, loader=loader_type, trait_dir=trait_dir)
             loader.load_traits()
     elif loader_type == "study_info":
             loader = Loader(tsv=ss_file, qtl_group=qtl_group, study=study, sqldb=database, loader=loader_type)
