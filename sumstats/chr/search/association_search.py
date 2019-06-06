1975d4fbab8b5c23edb78513a850e1b52ad83077
jdhayhurst <hayhurst.jd@gmail.com>
2019-06-06 15:37:03 +0100

    fixing loop breaking in searcher

--- sumstats/chr/search/association_search.py
+++ sumstats/chr/search/association_search.py
@@ -193,7 +193,6 @@ class AssociationSearch:
 
         self.datasets = self.df.to_dict(orient='list') if len(self.df.index) > 0 else self.datasets # return as lists - but could be parameterised to return in a specified format
         self.index_marker = self.starting_point + len(self.df.index)
-        print(self.datasets)
         return self.datasets, self.index_marker
         
 
