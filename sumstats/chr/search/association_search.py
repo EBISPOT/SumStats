cd8848fe5bde1a946d6c0416e5c9f0f7eda2da5f
jdhayhurst <hayhurst.jd@gmail.com>
2020-01-16 15:57:56 +0000

    debugging association search

--- sumstats/chr/search/association_search.py
+++ sumstats/chr/search/association_search.py
@@ -193,6 +193,7 @@ class AssociationSearch:
 
         self.datasets = self.df.to_dict(orient='list') if len(self.df.index) > 0 else self.datasets # return as lists - but could be parameterised to return in a specified format
         self.index_marker = self.starting_point + len(self.df.index)
+        logger.info(self.datasets)
         return self.datasets, self.index_marker
         
 
