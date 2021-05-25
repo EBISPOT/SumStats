392a0c54e89568567cebda3644ca67969944e7f0
jdhayhurst <hayhurst.jd@gmail.com>
2021-05-25 17:43:42 +0100

    adding hdf meta client, updating props and loader for

--- sumstats/load.py
+++ sumstats/load.py
@@ -1,19 +1,16 @@
 import sys
-import subprocess
-import glob
 import os
 import argparse
 import pandas as pd
-import tables as tb
 from sumstats.common_constants import *
 from sumstats.utils.properties_handler import properties
 from sumstats.utils import properties_handler
 from sumstats.utils import filesystem_utils as fsutils
-import sumstats.utils.sqlite_client as sq 
+import sumstats.utils.meta_client as mc
 
 
 class Loader():
-    def __init__(self, tsv, tsv_path, chr_dir, study_dir, study=None, trait=None, hdf_path=None, chromosome=None, sqldb=None, loader=None):
+    def __init__(self, tsv, tsv_path, chr_dir, study_dir, study=None, trait=None, hdf_path=None, chromosome=None, sqldb=None, loader=None, metafile=None):
         self.tsv = tsv
         self.study = study
         self.traits = trait
@@ -30,6 +27,7 @@ class Loader():
         self.ss_file = fsutils.get_file_path(path=self.tsv_path + "/{chrom}".format(chrom=self.chromosome), file=self.filename + ".csv") if loader in ['bychr', 'bystudy'] else fsutils.get_file_path(path=self.tsv_path, file=self.tsv)
 
         self.sqldb = sqldb
+        self.metafile = metafile
 
     def load_bychr(self):
         group = "chr" + self.chromosome
@@ -139,18 +137,12 @@ class Loader():
         self.load_study_filename()
 
     def load_study_and_trait(self):
-        sql = sq.sqlClient(self.sqldb)
-        for trait in self.traits:
-            data = [self.study, trait]
-            sql.cur.execute("insert or ignore into study_trait values (?,?)", data)
-            sql.cur.execute('COMMIT')
-
+        meta = mc.metaClient(self.metafile)
+        meta.load_study_trait(self.study, self.traits)
 
     def load_study_filename(self):
-        sql = sq.sqlClient(self.sqldb)
-        data = [self.study, self.filename]
-        sql.cur.execute("insert or ignore into study values (?,?)", data)
-        sql.cur.execute('COMMIT')
+        meta = mc.metaClient(self.metafile)
+        meta.load_study_filename(self.study, self.filename)
 
     @staticmethod
     def coerce_floats(value):
@@ -163,25 +155,6 @@ class Loader():
         except ValueError:
             return float('NaN')
         return value
-
-
-   # def reindex_files(self):
-   #     hdfs = fsutils.get_h5files_in_dir(path=self.hdfs_path, dir_name=self.chr_dir)
-   #     for f in hdfs:
-   #         with pd.HDFStore(f) as store:
-   #             group = store.keys()[0]
-   #             self.create_index(f, TO_INDEX, group)
-   #             self.create_cs_index(f, BP_DSET, group)
-
-
-   # def create_index(self, hdf, fields, group):
-   #     with pd.HDFStore(hdf) as store:
-   #         store.create_table_index(group, columns=fields, optlevel=6, kind='medium')
-
-
-   # def create_cs_index(self, hdf, fields, group):
-   #     with pd.HDFStore(hdf) as store:
-   #         store.create_table_index(group, columns=fields, optlevel=9, kind='full')
 
 
 
@@ -198,6 +171,7 @@ def main():
     h5files_path = properties.h5files_path # pragma: no cover
     tsvfiles_path = properties.tsvfiles_path  # pragma: no cover
     database = properties.sqlite_path
+    metafile = properties.meta_path
     chr_dir = properties.chr_dir
     study_dir = properties.study_dir
 
@@ -212,16 +186,16 @@ def main():
         if chromosome is None:
             print("You must specify the '-chr'...exiting")
         else:    
-            loader = Loader(filename, tsvfiles_path, chr_dir, study_dir, study, traits, h5files_path, chromosome, database, loader_type)
+            loader = Loader(filename, tsvfiles_path, chr_dir, study_dir, study, traits, h5files_path, chromosome, database, loader_type, metafile)
             loader.load_bychr()
     elif loader_type == 'bystudy':
         if chromosome is None:
             print("You must specify the '-chr'...exiting")
         else:
-            loader = Loader(filename, tsvfiles_path, chr_dir, study_dir, study, traits, h5files_path, chromosome, database, loader_type)
+            loader = Loader(filename, tsvfiles_path, chr_dir, study_dir, study, traits, h5files_path, chromosome, database, loader_type, metafile)
             loader.load_bystudy()
     elif loader_type == "study_info":
-        loader = Loader(filename, tsvfiles_path, chr_dir, study_dir, study, traits, h5files_path, chromosome, database, loader_type)
+        loader = Loader(filename, tsvfiles_path, chr_dir, study_dir, study, traits, h5files_path, chromosome, database, loader_type, metafile)
         loader.load_study_info()
     else:
         print("You must specify the '-loader'...exiting")
--- sumstats/utils/meta_client.py
+++ sumstats/utils/meta_client.py
@@ -6,9 +6,44 @@ class metaClient():
         self.metafile = metafile
 
 
+    """ LOAD STATEMENTS """
+
+    def load_study_trait(self, study, trait_list):
+        study_list = [study for _ in trait_list]
+        data_dict = {'study':study_list, 'trait': trait_list}
+        df = pd.DataFrame(data_dict)
+        df.to_hdf("meta.h5",
+                  key="study_trait",
+                  format='table',
+                  append=True,
+                  data_columns=list(df.columns.values),
+                  index=False)
+        with tb.open_file("meta.h5", "a") as hdf:
+            col = hdf.root['study_trait'].table.cols._f_col('study')
+            col.create_index(optlevel=6, kind="medium")
+            col = hdf.root['study_trait'].table.cols._f_col('trait')
+            col.create_index(optlevel=6, kind="medium")
+
+    def load_study_filename(self, study, filename):
+        data_dict = {'study': study, 'trait': filename}
+        df = pd.DataFrame(data_dict)
+        df.to_hdf("meta.h5",
+                  key="study",
+                  format='table',
+                  append=True,
+                  data_columns=list(df.columns.values),
+                  index=False)
+        with tb.open_file("meta.h5", "a") as hdf:
+            col = hdf.root['study'].table.cols._f_col('study')
+            col.create_index(optlevel=6, kind="medium")
+
+
+    """ SELECT STATEMENTS """
+
+
     def get_chr_pos(self, snp):
         data = []
-        search_string = "rsid == {snp}".format(snp)
+        search_string = "rsid == {snp}".format(snp=snp)
         with pd.HDFStore(self.metafile) as store:
             results = store.select('snp', where=search_string)
             data.append((results['chr'].values[0], results['position'].values[0]))
@@ -17,28 +52,11 @@ class metaClient():
         else:
             return False
 
-    def get_uuid_from_study(self, study):
-        uuids = []
-        for row in self.cur.execute("select * from uuid join study on uuid.study_id = study.rowid where study.study =?", (study,)):
-            uuids.append(row[0])
-        if uuids:
-            return uuids
-        else:
-            return False
-
-    def get_uuid_from_trait(self, trait):
-        uuids = []
-        for row in self.cur.execute("select * from uuid join trait on uuid.trait_id = trait.rowid where trait.trait =?", (trait,)):
-            uuids.append(row[0])
-        if uuids:
-            return uuids
-        else:
-            return False
-
     def get_studies(self):
         data = []
-        for row in self.cur.execute("SELECT study FROM study_trait"):
-            data.append(row[0])
+        with pd.HDFStore(self.metafile) as store:
+            results = store.select('study_trait')
+            data.extend(results['study'].tolist())
         if data:
             return data
         else:
@@ -46,8 +64,9 @@ class metaClient():
 
     def get_traits(self):
         data = []
-        for row in self.cur.execute("SELECT trait FROM study_trait"):
-            data.append(row[0])
+        with pd.HDFStore(self.metafile) as store:
+            results = store.select('study_trait')
+            data.extend(results['trait'].tolist())
         if data:
             return data
         else:
@@ -55,25 +74,55 @@ class metaClient():
 
     def get_studies_for_trait(self, trait):
         data = []
-        for row in self.cur.execute("select study from study_trait where trait =?", (trait,)):
-            data.append(row[0])
-        return data
+        search_string = "trait == {trait}".format(trait=trait)
+        with pd.HDFStore(self.metafile) as store:
+            results = store.select('study_trait', where=search_string)
+            data.extend(results['study'].tolist())
+        if data:
+            return data
+        else:
+            return False
 
     def get_traits_for_study(self, study):
         data = []
-        for row in self.cur.execute("select trait from study_trait where study =?", (study,)):
-            data.append(row[0])
-        return data
+        search_string = "study == {study}".format(study=study)
+        with pd.HDFStore(self.metafile) as store:
+            results = store.select('study_trait', where=search_string)
+            data.extend(results['trait'].tolist())
+        if data:
+            return data
+        else:
+            return False
 
 
     def get_file_id_for_study(self, study):
         data = []
-        for row in self.cur.execute("select file_id from study where study =?", (study,)):
-            data.append(row[0])
-        return data
+        search_string = "study == {study}".format(study=study)
+        with pd.HDFStore(self.metafile) as store:
+            results = store.select('study', where=search_string)
+            data.extend(results['file_id'].tolist())
+        if data:
+            return data
+        else:
+            return False
 
     def get_file_id_for_trait(self, trait):
         data = []
-        for row in self.cur.execute("select file_id from study join study_trait on study.study = study_trait.study where trait =?", (trait,)):
-            data.append(row[0])
+        search_string = "trait == {trait}".format(trait=trait)
+        with pd.HDFStore(self.metafile) as store:
+            study_list = store.select('study_trait', where=search_string)['study'].tolist()
+            for study in study_list:
+                search_string = "study == {study}".format(study=study)
+                data.extend(store.select('study', where=search_string)['file_id'].tolist())
         return data
+
+    #""" OTHER STATEMENTS """
+
+    #def commit(self):
+    #    self.cur.execute("COMMIT")
+
+    #def drop_rsid_index(self):
+    #    self.cur.execute("DROP INDEX rsid_idx")
+
+    #def create_rsid_index(self):
+    #    self.cur.execute("CREATE INDEX rsid_idx on snp (rsid)")
